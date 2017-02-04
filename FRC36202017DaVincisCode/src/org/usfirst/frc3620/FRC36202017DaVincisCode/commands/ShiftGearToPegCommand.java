a39f12841754bc0ef27930ea8e435138d2f581ad
fovea1959 <dwegscheid@sbcglobal.net>
2017-02-04 13:46:26 -0500

    Commit embroynic auto gear code.

--- FRC36202017DaVincisCode/src/org/usfirst/frc3620/FRC36202017DaVincisCode/commands/ShiftGearToPegCommand.java
+++ FRC36202017DaVincisCode/src/org/usfirst/frc3620/FRC36202017DaVincisCode/commands/ShiftGearToPegCommand.java
@@ -25,7 +25,7 @@ public class ShiftGearToPegCommand extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return Robot.gearSubsystem.gearIsAligned();
     }
 
     // Called once after isFinished returns true
