d74d43f447b0c5357fd6dfafbb7ebfde6ba8c3dd
Rahsaan <thecleophas22@gmail.com>
2017-02-06 20:22:41 -0500

    AutoTesting is finished

---
+++ FRC36202017DaVincisCode/src/org/usfirst/frc3620/FRC36202017DaVincisCode/commands/AutomatedMoveTimed.java
@@ -0,0 +1,102 @@
+package org.usfirst.frc3620.FRC36202017DaVincisCode.commands;
+
+import org.slf4j.Logger;
+import org.usfirst.frc3620.FRC36202017DaVincisCode.Robot;
+import org.usfirst.frc3620.FRC36202017DaVincisCode.RobotMap;
+import org.usfirst.frc3620.logger.EventLogging;
+import org.usfirst.frc3620.logger.EventLogging.Level;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ *
+ */
+public class AutomatedMoveTimed extends Command implements PIDOutput{
+	
+	Logger logger = EventLogging.getLogger(getClass(), Level.INFO);
+	
+
+	//Last working kP = .05
+	static final double kP = .05;
+	//Last working kI = .0015
+	static final double kI = .0015;	
+	//Last working kD = .02
+	static final double kD = .02;
+	
+	static final double kF = .00;
+	double sideStick;
+	
+	double howLongWeWantToMove = 0;
+	double howFastToMove = 0;
+	
+	Timer timer = new Timer();
+	
+	PIDController pidDriveStraight = new PIDController(kP, kI, kD, kF, Robot.driveSubsystem.getAhrs(), this);
+	
+
+    public AutomatedMoveTimed(double howLongInSeconds, double howFast) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.driveSubsystem);
+    	pidDriveStraight.setOutputRange(-1, 1);
+   
+    	howFastToMove = howFast;
+    	howLongWeWantToMove = howLongInSeconds;
+    }
+    
+    
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+
+    	logger.info("AutomatedMoveTimed start");
+    	RobotMap.driveSubsystemLeftEncoder.reset();
+    	RobotMap.driveSubsystemRightEncoder.reset();
+        pidDriveStraight.setSetpoint(Robot.driveSubsystem.getAutomaticHeading());
+    	pidDriveStraight.enable();
+    	
+    	timer.reset();
+    	timer.start();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.driveSubsystem.updateDashboardWithPidStuff(this, pidDriveStraight, sideStick);
+
+    	Robot.driveSubsystem.setDriveForward(-howFastToMove, sideStick);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	
+    	return timer.get() > howLongWeWantToMove;
+    	
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	logger.info("AutomatedMoveTimed end");
+    	pidDriveStraight.disable();
+    	Robot.driveSubsystem.stopMotors();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+        end();
+    }
+      
+    public void pidWrite(double output) {
+       sideStick = output;
+    }
+
+   
+}
---
+++ FRC36202017DaVincisCode/src/org/usfirst/frc3620/FRC36202017DaVincisCode/commands/AutomatedTurnCommand.java
@@ -0,0 +1,108 @@
+package org.usfirst.frc3620.FRC36202017DaVincisCode.commands;
+
+
+import org.slf4j.Logger;
+import org.usfirst.frc3620.FRC36202017DaVincisCode.Robot;
+import org.usfirst.frc3620.FRC36202017DaVincisCode.subsystems.DriveSubsystem;
+import org.usfirst.frc3620.logger.EventLogging;
+import org.usfirst.frc3620.logger.EventLogging.Level;
+
+import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
+
+/**
+ *
+ */
+public class AutomatedTurnCommand extends Command implements PIDOutput{
+	
+	Logger logger = EventLogging.getLogger(getClass(), Level.INFO);
+	
+	double sideStick;
+	
+	double howFarWeWantToTurn = 0;
+	
+	//PIDController pidTurn = new PIDController(.025, 00, 00, kF, ahrs, this); undershot
+	//PIDController pidTurn = new PIDController(.025, .001, 00, kF, ahrs, this); overshot
+	//PIDController pidTurn = new PIDController(.035, .001, 00, kF, ahrs, this); overshot
+	//PIDController pidTurn = new PIDController(.015, .001, 00, kF, ahrs, this); overshot
+	//PIDController pidTurn = new PIDController(.015, .0001, 00, kF, ahrs, this); works
+	PIDController pidTurn = new PIDController(.015, .0001, .00, .00, Robot.driveSubsystem.getAhrs(), this);
+	
+	public AutomatedTurnCommand() {
+		this(90.0);
+	}
+
+    public AutomatedTurnCommand(double howFar) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.driveSubsystem);
+    	
+    	pidTurn.setInputRange(0.0f,  360.0f);
+    	pidTurn.setOutputRange(-1, 1);
+    	pidTurn.setContinuous(true);
+    	
+    	howFarWeWantToTurn = howFar;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() 
+    {
+    	logger.info("AutomatedTurn start");
+    	double angle = Robot.driveSubsystem.getAutomaticHeading();
+    	double newAngle = Robot.driveSubsystem.changeAutomaticHeading(howFarWeWantToTurn);
+    	logger.info("angle was {}, new setpoint is {}", angle, newAngle);
+    	// TODO We need to look at this
+    	pidTurn.setSetpoint(newAngle);
+    	logger.info("we rechecked the setpoint = {}", pidTurn.getSetpoint());
+    	pidTurn.reset();
+    	pidTurn.setAbsoluteTolerance(10.0);
+    	pidTurn.enable();
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.driveSubsystem.updateDashboardWithPidStuff(this, pidTurn, sideStick);
+
+    	Robot.driveSubsystem.setDriveForward(0, sideStick);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	// TODO figure out why this is broken
+        //return pidTurn.onTarget();
+    	double want = Robot.driveSubsystem.getAutomaticHeading();
+    	double got = Robot.driveSubsystem.getAngle();
+    	double error = DriveSubsystem.angleDifference(want, got);
+    	logger.info("want {}, got {}, error {}, ontarget {}, getAvgError {}, getError {}", want, got, error, pidTurn.onTarget(),
+    			pidTurn.getAvgError(),
+    			pidTurn.getError());
+    	
+    	return error < 10;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	logger.info("AutomatedTurn end");
+    	pidTurn.disable();
+    	Robot.driveSubsystem.stopMotors();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+    
+    public void pidWrite(double output) {
+       sideStick = output;
+    }
+
+   
+}
