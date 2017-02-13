46c8c4c3000451fe61b0e7439218b453e0301382
fovea1959 <dwegscheid@sbcglobal.net>
2017-02-13 18:46:17 -0500

    Add code to pull motor statistics. Start CameraServer.

--- FRC36202017DaVincisCode/src/org/usfirst/frc3620/misc/DPadTest.java
+++ FRC36202017DaVincisCode/src/org/usfirst/frc3620/misc/DPadTest.java
@@ -2,8 +2,6 @@ package org.usfirst.frc3620.misc;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import edu.wpi.first.wpilibj.GenericHID;
