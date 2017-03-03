73ec5090f405f1aaffff593ce38744d060c25e25
slipshot711 <vameresh@gmail.com>
2017-03-02 20:37:43 -0500

    Minor Change to Auto

--- FRC36202017DaVincisCode/src/org/usfirst/frc3620/FRC36202017DaVincisCode/commands/AutoMoveDownFieldCommand.java
+++ FRC36202017DaVincisCode/src/org/usfirst/frc3620/FRC36202017DaVincisCode/commands/AutoMoveDownFieldCommand.java
@@ -19,7 +19,7 @@ public class AutoMoveDownFieldCommand extends CommandGroup {
         //      addSequential(new Command2());
         // Command1 and Command2 will run in parallel.
     	addSequential(new AutoBackUpFromPegCommand());
-    	addSequential(new AutomatedTurnCommand(60));
+    	addSequential(new AutomatedTurnCommand(-60));
     	addSequential(new AutomatedMoveTimedCommand(15, .85));
     	// A command group will require all of the subsystems that each member
         // would require.
