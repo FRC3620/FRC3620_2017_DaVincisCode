1341cabb15121926025d6cc0fc6d66f27cd66bff
chaynes2019 <chaynes2019@gmail.com>
2017-03-16 20:47:05 -0400

    Made new three wheel shooter Branch

---
+++ FRC36202017DaVincisCode/src/org/usfirst/frc3620/FRC36202017DaVincisCode/subsystems/NewShooterSubsystem.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc3620.FRC36202017DaVincisCode.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class NewShooterSubsystem extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
